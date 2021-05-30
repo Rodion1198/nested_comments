0133a093857e30a802c874f18bf0d10d9ae82053
Rodion1198 <rodka1198@gmail.com>
2021-05-30 20:33:37 +0300

    added the ability to create comment and reply to already created comment

--- comments/templatetags/comments_tree.py
+++ comments/templatetags/comments_tree.py
@@ -7,17 +7,35 @@ register = Library()
 @register.filter
 def comments_filter(comments_list):
     res = """
-            <ul>
-            {}
+            <ul style="list-style-type:none;">
+              <div class="col-md-12 mt-2">
+                    {}
+              </div>
             </ul>
           """
     i = ''
     for comment in comments_list:
         i += """
             <li>
-            {comment_id}
+               <div class="col-md-12 mb-2 mt-2 p-0">
+                    <small>{author}</small> | pub: {timestamp}
+                    <hr>
+                    <p>{text} | id={id}</p>
+                    <a href="#" class="reply" data-id="{id}" data-parent={parent_id}>Reply</a>
+                    <form action="" method="POST" class="comment-form form-group" id="form-{id}" style="display: none;">
+                        <textarea type="text" class="form-control" name="comment-text"></textarea><br>
+                        <input type="submit" class="btn btn-primary submit-reply" data-id="{id}"
+                        data-submit-reply="{parent_id}" value="Post">
+                    </form>
+                </div>
             </li>
-             """.format(comment_id=comment['id'])
+             """.format(
+                        id=comment['id'],
+                        author=comment['author'],
+                        timestamp=comment['timestamp'],
+                        text=comment['text'],
+                        parent_id=['parent_id']
+                    )
         if comment.get('children'):
             i += comments_filter(comment['children'])
     return mark_safe(res.format(i))
--- comments/urls.py
+++ comments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import base_view
+from .views import base_view, create_child_comment, create_comment
 
 urlpatterns = [
     path('post-comments/', base_view),
+    path('create_comment', create_comment, name='comment_create'),
+    path('create_child_comment', create_child_comment, name='comment_child_create'),
 ]
